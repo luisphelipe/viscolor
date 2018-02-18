e8f194a080cfa0537152b399679274d4be853ac2
Luis Phelipe <luisphelipecode@gmail.com>
2018-02-18 00:35:03 -0300

    Fix code generator and html layout

--- viscolor.py
+++ viscolor.py
@@ -3,64 +3,36 @@ import subprocess
 
 
 color_path = "/home/automata/.cache/wal/colors"
-htmltemplate = "<!DOCTYPE html>\n\
+
+html_top = "<!DOCTYPE html>\n\
 \n\
 <html>\n\
     <head>\n\
 	<meta charset=\"UTF-8\">\n\
 	<title>color-scheme</title>\n\
+        <style>\n\
+            * {\n\
+                font-family: monospace;\n\
+                margin: 0;\n\
+                padding: 0;\n\
+            }\n\
+            body {\n\
+                margin-top: 4px;\n\
+            }\n\
+            div {\n\
+                margin: 2px auto;\n\
+                width: 1300px;\n\
+                height: 40px;\n\
+            }\n\
+        </style>\n\
     </head>\n\
-    <body style=\"color:white;\">\n\
-	<div style=\"width:1332px;\n\
-	    height:30px;\n\
-            background:{0};\"> <h3>00 {0}</div>\n\
-	<div style=\"width:1332px;\n\
-	    height:30px;\n\
-	    background:{1};\"> <h3>01 {1}</div>\n\
-	<div style=\"width:1332px;\n\
-	    height:30px;\n\
-	    background:{2};\"> <h3>02 {2}</div>\n\
-	<div style=\"width:1332px;\n\
-	    height:30px;\n\
-	    background:{3};\"> <h3>03 {3}</div>\n\
-	<div style=\"width:1332px;\n\
-	    height:30px;\n\
-	    background:{4};\"> <h3>04 {4}</div>\n\
-	<div style=\"width:1332px;\n\
-	    height:30px;\n\
-	    background:{5};\"> <h3>05 {5}</div>\n\
-	<div style=\"width:1332px;\n\
-	    height:30px;\n\
-	    background:{6};\"> <h3>06 {6}</div>\n\
-	<div style=\"width:1332px;\n\
-	    height:30px;\n\
-	    background:{7};\"> <h3>07 {7}</div>\n\
-	<div style=\"width:1332px;\n\
-	    height:30px;\n\
-	    background:{8};\"> <h3>07 {8}</div>\n\
-	<div style=\"width:1332px;\n\
-	    height:30px;\n\
-	    background:{9};\"> <h3>09 {9}</div>\n\
-	<div style=\"width:1332px;\n\
-	    height:30px;\n\
-	    background:{10};\"> <h3>10 {10}</div>\n\
-	<div style=\"width:1332px;\n\
-	    height:30px;\n\
-	    background:{11};\"> <h3>11 {11}</div>\n\
-	<div style=\"width:1332px;\n\
-	    height:30px;\n\
-	    background:{12};\"> <h3>12 {12}</div>\n\
-	<div style=\"width:1332px;\n\
-	    height:30px;\n\
-	    background:{13};\"> <h3>13 {13}</div>\n\
-	<div style=\"width:1332px;\n\
-	    height:30px;\n\
-	    background:{14};\"> <h3>14 {14}</div>\n\
-	<div style=\"width:1332px;\n\
-	    height:30px;\n\
-	    background:{15};\"> <h3>15 {15}</div>\n\
-    </body>\n\
+    <body>"
+
+html_div ="\n       <div style=\"background:{0};\"><h3>{1:02d} {0}</h3></div>"
+
+html_bot = "\n    </body>\n\
 </html>"
+
 
 def load_color_scheme():
     colors = []
@@ -76,24 +48,12 @@ def main():
         print("{:02d} {}".format(n, c))
         n += 1
 
-    template = htmltemplate.format(
-            color_scheme[0],
-            color_scheme[1],
-            color_scheme[2],
-            color_scheme[3],
-            color_scheme[4],
-            color_scheme[5],
-            color_scheme[6],
-            color_scheme[7],
-            color_scheme[8],
-            color_scheme[9],
-            color_scheme[10],
-            color_scheme[11],
-            color_scheme[12],
-            color_scheme[13],
-            color_scheme[14],
-            color_scheme[15]
-            )
+    template = html_top
+    for i in range(len(color_scheme)):
+        template += html_div.format(color_scheme[i].upper(), i)
+
+    template += html_bot
+
     with open("htmlvisualizer.html", 'w') as htmlfile:
         htmlfile.write(template)
 
